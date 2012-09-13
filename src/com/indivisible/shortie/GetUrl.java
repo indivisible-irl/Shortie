0ac8fc8c353ac3d9e8bf101d494d4f1c27605413
indivisible-irl <mbspare@gmail.com>
2012-09-13 19:33:56 +0100

    sync

--- src/com/indivisible/shortie/GetUrl.java
+++ src/com/indivisible/shortie/GetUrl.java
@@ -1,5 +1,28 @@
 package com.indivisible.shortie;
 
-public class GetUrl {
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GetUrl {
+	private final URL googleUrl = new URL("https://www.googleapis.com/urlshortener/v1/url");
+	private JSONObject request = new JSONObject();
+	private final String authKey = "AIzaSyAWCd8ONumLwxv0RqthcHXjcD1OlhXgrlE";	
+	
+	public GetUrl(String longUrl){
+		try {
+			request.put("longUrl", longUrl);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String shortenUrl(){
+		HttpURLConnection httpConn = new HttpURLConnection(GoogleUrl);
+		
+		return null;
+	}
+	
 }
