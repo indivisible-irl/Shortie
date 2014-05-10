c54ec5f145c7318b7602ae7b15d7134da4f83d2b
indivisible-irl <github@indivisibleindustries.com>
2014-05-10 21:22:18 +0100

    start move to fragments

---
+++ src/com/indivisible/shortie/fragment/LinkPairListEmptyFragment.java
@@ -0,0 +1,7 @@
+package com.indivisible.shortie.fragment;
+
+
+public class LinkPairListEmptyFragment
+{
+
+}
---
+++ src/com/indivisible/shortie/fragment/LinkPairListFragment.java
@@ -0,0 +1,10 @@
+package com.indivisible.shortie.fragment;
+
+import android.support.v4.app.ListFragment;
+
+
+public class LinkPairListFragment
+        extends ListFragment
+{
+
+}
